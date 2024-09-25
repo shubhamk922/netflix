cdf27eeb901ecc5e4589bee6d717e568ff3d0f99
shubhamk922 <shubhamk922@gmail.com>
2024-09-25 21:32:01 +0530

    Commit 2

---
+++ foundation/logger/logger.go
@@ -0,0 +1,25 @@
+// This is a package used by all layers to log
+package logger
+
+type Logger struct {
+}
+
+func (logger *Logger) Infof() {
+
+}
+
+func (logger *Logger) Error() {
+
+}
+
+func (logger *Logger) Errorf() {
+
+}
+
+func (logger *Logger) Debug() {
+
+}
+
+func (logger *Logger) Warn() {
+
+}
