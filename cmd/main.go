c50a4e3633d5cde05b8afeba3996000438938dd0
shubhamk922 <shubhamk922@gmail.com>
2025-10-13 13:04:20 +0530

    Commit 3 change the default server to custom graceful server

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"example.com/netflix/internal/infrastructure/logger"
 	"example.com/netflix/internal/recommendation/adapter/in/controller"
@@ -10,6 +14,44 @@ import (
 	"example.com/netflix/internal/recommendation/application/service"
 	"example.com/netflix/internal/recommendation/domain"
 )
+
+type GracefulServer struct {
+	Server   *http.Server
+	Listener net.Listener
+}
+
+func NewServer(port string) *GracefulServer {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	return &GracefulServer{Server: server}
+}
+
+func (s *GracefulServer) start() error {
+	listener, err := net.Listen("tcp", s.Server.Addr)
+
+	if err != nil {
+		return err
+	}
+	s.Listener = listener
+	go s.Server.Serve(s.Listener)
+	// log
+	return nil
+}
+
+func (s *GracefulServer) shutdown() error {
+	if s.Listener != nil {
+		err := s.Listener.Close()
+		// we can check for all resource conection if that is close or not to gracefully shutdown
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func main() {
 
@@ -19,15 +61,29 @@ func main() {
 	usercase := service.NewRecommendationService(domain_service, repo, logger)
 	c := controller.NewRecommendationController(usercase)
 
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/movies/title/:id", c.GetAllSimilarMovies)
-
-	err := http.ListenAndServe(":8080", mux)
+	port := "8080"
+	server := NewServer(port)
+	err := server.start()
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("error is starting server %+v", err)
 	}
 
-	fmt.Printf("Server is Listening on port 8080")
+	log.Printf("Server is lsitening on port %s ", port)
+
+	handler := server.Server.Handler.(*http.ServeMux)
+	handler.HandleFunc("/movies/title/:id", c.GetAllSimilarMovies)
+
+	// so as to avoid shutdown the main function
+	done := make(chan struct{})
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+	// Now Lets write a graceful shutdown function
+	go func() {
+		<-interrupt
+		server.shutdown()
+		done <- struct{}{}
+	}()
+
+	<-done
 }
